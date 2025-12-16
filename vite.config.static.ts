c9ebbeda77e4a21ed3b95ac02bca71de98c03c27
Ernesto Bellei <ernesto@hund.studio>
2025-12-16 13:54:36 +0100

    Update version to 0.0.0-beta.1 and inject version into static assets.

--- vite.config.static.ts
+++ vite.config.static.ts
@@ -1,8 +1,27 @@
 // vite.config.app2.js
-import { defineConfig } from "vite";
+import { defineConfig, Plugin } from "vite";
 import { resolve } from "path";
+import { readFileSync } from "fs";
+
+const pkg = JSON.parse(readFileSync(resolve("package.json"), "utf-8"));
+const version = pkg.version;
+
+const versionPlugin = (): Plugin => {
+  return {
+    name: "html-version",
+    transformIndexHtml(html) {
+      const scriptRegex = /(\/assets\/main\.js)/;
+      const cssRegex = /(\/assets\/static\.css)/;
+      const newHtml = html
+        .replace(scriptRegex, `$1?v=${version}`)
+        .replace(cssRegex, `$1?v=${version}`);
+      return newHtml;
+    },
+  };
+};
 
 export default defineConfig({
+  plugins: [versionPlugin()],
   build: {
     outDir: "src/static",
     emptyOutDir: true,
